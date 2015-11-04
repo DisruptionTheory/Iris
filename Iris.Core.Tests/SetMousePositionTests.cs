7f2f78c5019de497864bcca4062a616adbdfb781
Eric Wehrly <eric@disruptiontheory.com>
2015-11-03 20:52:02 -0500

    Added a testy test.

---
+++ Iris.Core.Tests/SetMousePositionTests.cs
@@ -0,0 +1,21 @@
+﻿using Iris.Core;
+using Iris.Infrastructure.Models;
+using NUnit.Framework;
+
+namespace Iris.Core.Tests
+{
+    public class SetMousePositionTests
+    {
+        [Test]
+        public void ATestyTest()
+        {
+            MousePosition postion = new MousePosition();
+
+            postion.X = 10;
+
+            postion.Y = 10;
+
+            IrisCore.MouseService.SetMousePosition(postion);
+        }
+    }
+}
